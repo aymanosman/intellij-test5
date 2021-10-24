729fb9a2573e503ba273fc915a01121b8178c7fa
GitHub Action <action@github.com>
2021-10-24 14:29:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/aymanosman/intellijtest5/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/aymanosman/intellijtest5/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.aymanosman.intellijtest5.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.aymanosman.intellijtest5.MyBundle
 
 class MyApplicationService {
 
